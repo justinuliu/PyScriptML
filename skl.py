6463233f355a526caa2f8cfc82fa358bc21e5a71
Justin Liu <justin.l@waikato.ac.nz>
2022-08-25 17:04:43 +1200

    Fix some issues.

--- skl.py
+++ skl.py
@@ -7,18 +7,18 @@ from sklearn.model_selection import cross_val_score
 
 pn.config.sizing_mode = 'stretch_width'
 
-ds_opts = {'iris': datasets.load_iris, 'digits': datasets.load_digits}
+ds_opts = {'diabetes': datasets.load_diabetes, 'iris': datasets.load_iris}
 algorithms = {'LinearRegression': linear_model.LinearRegression, 'SVC': svm.SVC}
-evaluators = ['accuracy', 'adjusted_mutual_info_score', 'neg_mean_absolute_error']
+evaluators = ['neg_mean_absolute_error', 'accuracy']
 
 ds_widgets = pn.widgets.Select(name='Dataset', options=list(ds_opts.keys())).servable(target='ds-widget')
 algo_widgets = pn.widgets.Select(name='Algorithms', options=list(algorithms.keys())).servable(target='algo-widget')
-eval_widgets = pn.widgets.Select(name='Evaluator', options=evaluators).servable(target='eval-widget')
+metrics_widgets = pn.widgets.Select(name='Metrix', options=evaluators).servable(target='metrics-widget')
 cv_widgets = pn.widgets.IntSlider(name='Folds', start=1, end=10, value=5).servable(target='cv-widget')
 
 
 def get_settings():
-    return f'Dataset: {ds_widgets.value}\nAlgorithm: {algo_widgets.value}\nEvaluator: {eval_widgets.value}\n'
+    return f'Dataset: {ds_widgets.value}\nAlgorithm: {algo_widgets.value}\nEvaluator: {metrics_widgets.value}\n'
 
 
 settings = pn.pane.Str(get_settings()).servable(target='settings')
@@ -30,7 +30,7 @@ def train(event=None):
     ds = ds_opts[ds_widgets.value]()
     model = algorithms[algo_widgets.value]()
 
-    cv_result = cross_val_score(model, ds.data, ds.target, cv=cv_widgets.value, scoring=eval_widgets.value)
+    cv_result = cross_val_score(model, ds.data, ds.target, cv=cv_widgets.value, scoring=metrics_widgets.value)
 
     result.object = cv_result
 
